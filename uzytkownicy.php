3ccccb99a02bd60350f8510f566ec1e27c552c97
Maria Filipowicz <106813606+Kodokodesu@users.noreply.github.com>
2023-03-13 10:32:45 +0100

    Add files via upload

---
+++ uzytkownicy.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <title>Użytkownicy</title>
+</body>
+<?php
+require_once "./skrypty_db/connect.php";
+echo $conn->connect_errno;
+?>
+</html>
